068041c782d4edc8fb9a2b4d2afed28fffed4a21
Lark-Liang <larkliang@foxmail.com>
2025-12-14 15:47:13 +0800

    Initial commit

---
+++ src/main/java/com/example/ticketsystem/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.example.ticketsystem.dto;
+
+import lombok.Data;
+
+/**
+ * @author Lark
+ * @ date 2025/12/11  20:46
+ * @ description
+ */
+@Data
+public class LoginRequest {
+    private String username;
+    private String password;
+}
